9d1b7db0cc62d6c3dafe9707e9444c18b183c7dd
2JongHyeok <104877754+2JongHyeok@users.noreply.github.com>
2025-09-02 21:14:51 +0900

    feat: Enemy AI 스크립트 추가

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] protected Rigidbody rb;
+
+    public int Hp = 100;
+
+    protected const string ISCHASE = "IsChase";
+    protected const string ISAttack = "IsAttack";
+
+    public float attackRange = 2f;       // 공격 사거리
+
+    protected Transform repairKit;      
+    protected float attackCooldown;      // 공격 쿨타임
+    public float attackDelay = 3f;
+
+    public float speed = 5.0f;
+    public float moveRotationSpeed = 10.0f;
+
+    protected bool isDamaged = false; // 지금 맞은 상태인가
+    //private float curInvincibleTimer = 0f;
+
+    protected Coroutine rotateCoroutine;
+
+    protected bool isDie = false;
+
+    protected virtual void FixedUpdate()
+    {
+        if (isDie) return;
+
+        float distance = Vector3.Distance(transform.position, repairKit.position);
+        if(attackCooldown < 0) {
+            attackCooldown = attackDelay;
+            Attack();
+        } else {
+            Vector3 dir = repairKit.transform.position - transform.position;
+            rb.MovePosition(rb.position + dir.normalized * speed * Time.fixedDeltaTime);
+
+            Quaternion targetRotation = Quaternion.LookRotation(dir, transform.up);
+            rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, moveRotationSpeed * Time.fixedDeltaTime));
+        }
+    }
+
+    protected virtual void Update()
+    {
+        attackCooldown -= Time.deltaTime;
+    }
+
+    protected virtual void Attack()
+    {
+        // 공격 어떻게 할건지 넣기
+    }
+
+    protected virtual void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //Tapasse12_PlayerController.Instance.GetDamage(bodyDamage);
+        }
+    }
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerHand"))
+        {
+            if (isDamaged) return;
+
+            //Hp -= Tapasse12_PlayerController.Instance.Damage;
+            isDamaged = true;
+
+            if (Hp <= 0)
+            {
+                isDie = true;
+                //Tapasse12_IngameManager.Instance.GetItem(transform);
+
+                //Tapasse12_IngameManager.Instance.UpdateDoor();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
