58ac7efa75e5b3d404c157725e729f8510b50c26
2JongHyeok <104877754+2JongHyeok@users.noreply.github.com>
2025-09-04 10:02:06 +0900

    fix: 체력이 업데이트 안되던 버그 수정

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -431,6 +431,7 @@ public class PlayerController : MonoBehaviour
     {
         maxHp += hpUpgrade;
         hp = maxHp;
+        UpdateVisual();
     }
 
     private IEnumerator InvincibleBlink()
