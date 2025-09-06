3e94501a5674d62658483a03f32ad237aa95061a
2JongHyeok <104877754+2JongHyeok@users.noreply.github.com>
2025-09-06 11:27:31 +0900

    벽 뚫는 문제 해결

--- Assets/Scripts/Mineral/MineralHpBarBillboardScript.cs
+++ Assets/Scripts/Mineral/MineralHpBarBillboardScript.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
 
 public class MineralHpBarBillboardScript : MonoBehaviour
-{ 
+{
+    public Transform target;          // Mineral Blue 오브젝트
+
     void LateUpdate()
     {
-        // 카메라 바라보기
-        transform.LookAt(Camera.main.transform);
-        // 필요하다면 뒤집기
-        transform.Rotate(0, 180, 0);
+        if (target == null) return;
+
+        // Mineral 머리 위 위치
+        target = GetComponentInParent<MineralSpawnManager>().transform;
+
+        Quaternion targetRotation = target.rotation;
+
+        // 카메라 쪽으로 조금
+        Quaternion rotation = Quaternion.AngleAxis(180, transform.forward);
+        // 2. Rigidbody의 회전 적용
+        transform.rotation = target.rotation * rotation;
+
+        // UI(Canvas)는 항상 카메라 쪽을 바라봐야 함
+        transform.forward = Camera.main.transform.forward;
     }
 }
